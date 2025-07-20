8c62f75a736c3c402f2cac9ecd8925b56b4a003a
Lilitr <liliananathaly1409@gmail.com>
2025-07-20 17:36:14 -0400

    Working on a Library Manager class

--- src/core/books.py
+++ src/core/books.py
@@ -1,7 +1,7 @@
 import datetime
 
 class Book(object):
-    def __init__(self, title, author, year, gender):
+    def __init__(self, title:str, author:str, year:int, gender:str):
         self.ID = None
         self.title = title
         self.author = author
@@ -11,12 +11,25 @@ class Book(object):
         self.loaned_to = None
         
     def __str__(self):
-        return f"New Book: {self.title} | {datetime.datetime.date()}"
+        return f"New Book: {self.title} | {datetime.datetime.date()}" # Acortar el formato de la fecha
         
     def change_status(self, new_status):
         self.status = new_status
         if new_status == "available":
             self.loaned_to = None
             
-book = Book("my book", "me", 2019, "romance")
-print(book)
+class LibraryManager(object):
+    def __init__(self, database):
+        self.database = database
+    
+    def add_book(self, book:Book):
+        pass # Ver como implementar esta logica
+    
+    def find_by_author(self, author:str):
+        pass # Ver como implementar esta logica
+    
+    def delete_book(self, id:int):
+        pass # Ver como implementar esta logica
+    
+    def get_book_by_ID(self, id:int):
+        pass # Ver como implementar esta logica
