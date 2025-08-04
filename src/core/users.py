97c0a9e9376b312d403d14aff1165363b54af4be
Lilitr <liliananathaly1409@gmail.com>
2025-08-03 21:32:31 -0400

    working on the user.py file. Some issues solved

--- src/core/users.py
+++ src/core/users.py
@@ -7,6 +7,7 @@ class User(object):
     
     def __init__(self, name:str, sex:str, email:str, phone:str):
         self.id = f"USER-{User.user_counter:03d}"
+        User.user_counter += 1
         self.name = name
         self.sex = sex 
         self.email = email
@@ -19,13 +20,13 @@ class User(object):
         """Converts the User object into a dictionary"""
         return {
             "id": self.id,
-            "title": self.title,
-            "author": self.author,
-            "year": self.year,
-            "gender": self.gender,
-            "status": self.status,
-            "loaned_to": self.loaned_to,
-            "created_at": self.created_at
+            "name": self.name,
+            "sex": self.sex,
+            "email": self.email,
+            "phone": self.phone,
+            "active": self.active,
+            "joined_date": self.joined_date,
+            "borrowed_books": self.borrowed_books
         }
     
     def borrow_book(self, book_id):
@@ -56,9 +57,15 @@ class UserManager(object):
         save_data(self.database)
         
     def find_user_by_email(self, email):
-        pass
-        # Ver como implementar esta logica
+        for user, user_info in self.database["users"].items():
+            if user_info["email"] == email:
+                return user
         
-    def get_user_activity(self):
-        pass
-        # Ver como implementar esta logica
+    def get_user_activity(self, user_id):
+        user = self.database["users"].get(user_id)
+        if user:
+            return {
+                "active": user["active"],
+                "borrowed_books": user["borrowed_books"],
+                "total_books": len(user["borrowed_books"])
+            }
